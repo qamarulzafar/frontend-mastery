cbdbdeebe922b5d8a61e2343eaf8c970238f36fb
qamar ul zafar siddiqui <qamarsiddiqui0311@gmail.com>
2025-03-26 03:02:30 +0500

    practicing_to_React_(Components, JSX, State, Props)

---
+++ first_month/day_03/vite-project/src/Components/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header className="">
+        <div className="log">
+            <h3>Qamar</h3>
+        </div>
+        <div className="nav">
+            <div>Home</div>
+            <div>About</div>
+            <div>Contact</div>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header
